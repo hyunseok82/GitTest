603e52a8635fa58f7ef4461c5785adf8ac7b3ed3
hyunseok82@sk.com <1100425@SKTX1100425MN01.local>
2016-08-17 09:43:22 +0900

    Create GitTest project.

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by 1100425 on 2016. 8. 17..
+//  Copyright © 2016년 SKTX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
